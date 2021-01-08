1975d9392b4d745ca3e2ebf8aadfd6aa2eecac7d
aurudin <renon@renon.com.br>
2021-01-08 17:06:54 -0300

    Pagina Proposta

---
+++ pages/proposta-de-criacao-de-marca.vue
@@ -0,0 +1,447 @@
+<template>
+  <main class="fundo-propostas">    
+    <div class="grid-x grid-padding-x bg-propostas">
+      <div class="cell large-4 video">
+        <iframe src="https://player.vimeo.com/video/498353385" width="100%" height="860" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+      </div>
+      <div class="cell large-8 proposta-criacao">
+        <h1>Proposta de Criação de Marca Profissional</h1>
+        <p class="text-proposta">Nós da CriaçãodeMarcas.com somos especializados em criação de marcas para empresas, produtos e serviços. Nosso trabalho é proporcionar um serviço de qualidade para impactar e estimular seu público alvo na geração de novos negócios.</p>
+        <div class="grid-x grid-padding-x informacoes">
+          <div class="cell large-4">
+            <h3>O que está incluso:</h3>
+            <p>
+              <picture>
+                <source
+                  srcset="~/assets/img/webp/icon-condicao-1.webp" type="image/webp" />
+                <source srcset="~/assets/img/icon-condicao-1.png" type="image/png" />
+                <img loading="lazy" src="~/assets/img/icon-condicao-1.png" alt />
+              </picture>
+              Ajustes ilimitados
+            </p>
+            <p>
+              <picture>
+                <source
+                  srcset="~/assets/img/webp/icon-condicao-7.webp" type="image/webp" />
+                <source srcset="~/assets/img/icon-condicao-7.png" type="image/png" />
+                <img loading="lazy" src="~/assets/img/icon-condicao-7.png" alt />
+              </picture>
+              Versões ilimitadas
+            </p>
+            <p>
+              <picture>
+                <source
+                  srcset="~/assets/img/webp/icon-condicao-2.webp" type="image/webp" />
+                <source srcset="~/assets/img/icon-condicao-2.png" type="image/png" />
+                <img loading="lazy" src="~/assets/img/icon-condicao-2.png" alt />
+              </picture>
+              Apresentação em até 72 horas
+            </p>
+            <p>
+              <picture>
+                <source
+                  srcset="~/assets/img/webp/icon-condicao-3.webp" type="image/webp" />
+                <source srcset="~/assets/img/icon-condicao-3.png" type="image/png" />
+                <img loading="lazy" src="~/assets/img/icon-condicao-3.png" alt />
+              </picture>
+              Inclui Manual de marca
+            </p>
+            <p>
+              <picture>
+                <source
+                  srcset="~/assets/img/webp/icon-condicao-4.webp" type="image/webp" />
+                <source srcset="~/assets/img/icon-condicao-4.png" type="image/png" />
+                <img loading="lazy" src="~/assets/img/icon-condicao-4.png" alt />
+              </picture>
+              Design 100% exclusivo
+            </p>
+            <p>
+              <picture>
+                <source
+                  srcset="~/assets/img/webp/icon-condicao-5.webp" type="image/webp" />
+                <source srcset="~/assets/img/icon-condicao-5.png" type="image/png" />
+                <img loading="lazy" src="~/assets/img/icon-condicao-5.png" alt />
+              </picture>
+              Formatos em Alta resolução
+            </p>
+            <p>
+              <picture>
+                <source
+                  srcset="~/assets/img/webp/icon-condicao-6.webp" type="image/webp" />
+                <source srcset="~/assets/img/icon-condicao-6.png" type="image/png" />
+                <img loading="lazy" src="~/assets/img/icon-condicao-6.png" alt />
+              </picture>
+              Profissionais especialistas
+            </p>
+          </div>
+          <div class="cell large-4 formatos">
+            <h3 class="desktop">Formatos:</h3>
+            <h3 class="mobile">Receba nos principais formatos:</h3>
+            <div class="tipos">
+              <p>
+                <picture>
+                  <source
+                    srcset="~/assets/img/video/webp/icon-proposta-1.webp" type="image/webp" />
+                  <source srcset="~/assets/img/video/icon-proposta-1.png" type="image/png" />
+                  <img loading="lazy" src="~/assets/img/video/icon-proposta-1.png" alt />
+                </picture>
+                <span>Imagem</span>
+              </p>
+              <p>
+                <picture>
+                  <source
+                    srcset="~/assets/img/video/webp/icon-proposta-2.webp" type="image/webp" />
+                  <source srcset="~/assets/img/video/icon-proposta-2.png" type="image/png" />
+                  <img loading="lazy" src="~/assets/img/video/icon-proposta-2.png" alt />
+                </picture>
+                <span>Fundo transparente</span>
+              </p>
+              <p>
+                <picture>
+                  <source
+                    srcset="~/assets/img/video/webp/icon-proposta-3.webp" type="image/webp" />
+                  <source srcset="~/assets/img/video/icon-proposta-3.png" type="image/png" />
+                  <img loading="lazy" src="~/assets/img/video/icon-proposta-3.png" alt />
+                </picture>
+                <span>CorelDraw</span>
+              </p>
+              <p>
+                <picture>
+                  <source
+                    srcset="~/assets/img/video/webp/icon-proposta-4.webp" type="image/webp" />
+                  <source srcset="~/assets/img/video/icon-proposta-4.png" type="image/png" />
+                  <img loading="lazy" src="~/assets/img/video/icon-proposta-4.png" alt />
+                </picture>
+                <span>Illustrator</span>
+              </p>
+              <p>
+                <picture>
+                  <source
+                    srcset="~/assets/img/video/webp/icon-proposta-5.webp" type="image/webp" />
+                  <source srcset="~/assets/img/video/icon-proposta-5.png" type="image/png" />
+                  <img loading="lazy" src="~/assets/img/video/icon-proposta-5.png" alt />
+                </picture>
+                <span>Apresentação</span>
+              </p>
+              <p>
+                <picture>
+                  <source
+                    srcset="~/assets/img/video/webp/icon-proposta-6.webp" type="image/webp" />
+                  <source srcset="~/assets/img/video/icon-proposta-6.png" type="image/png" />
+                  <img loading="lazy" src="~/assets/img/video/icon-proposta-6.png" alt />
+                </picture>
+                <span>Vetor</span>
+              </p> 
+            </div>           
+          </div>
+          <div class="cell large-4 adquira">
+            <h3>Adquira já</h3>
+            <p class="price"><span class="por">R$ 480,00</span></p>
+            <p class="parcelas">Parcele no cartão sem juros em 3x de R$ 160,00</p>
+            <a href="https://mpago.la/1DMNJq4" target="_blank" class="btn-compre">
+              Compre agora
+              <picture>
+                <source
+                  srcset="~/assets/img/video/webp/img-mercado-pago.webp" type="image/webp" />
+                <source srcset="~/assets/img/video/img-mercado-pago.png" type="image/png" />
+                <img loading="lazy" src="~/assets/img/video/img-mercado-pago.png" alt />
+              </picture>
+            </a>
+          </div>
+        </div>
+        <div class="grid-x rodape">
+          <div class="cell">
+            <a href="/">
+              <picture>
+                <source
+                  srcset="~/assets/img/video/webp/marca-proposta.webp" type="image/webp" />
+                <source srcset="~/assets/img/video/marca-proposta.png" type="image/png" />
+                <img loading="lazy" src="~/assets/img/video/marca-proposta.png" alt />
+              </picture>
+            </a>
+            <p>Av. Presidente Kennedy, 3425 | Água Verde | Curitiba-PR |  CEP: 80610-010 | CNPJ 20.012.672/0001-24</p>
+            <p>
+              <a href="https://api.whatsapp.com/send?phone=5541988492754" target="_blank">
+                <span
+                  class="iconify"
+                  data-icon="mdi:whatsapp"
+                  data-inline="false"
+                ></span>
+                41 98849.2754
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="grid-x mobile-rodape">
+        <div class="cell">
+          <p class="price"><span class="por">R$ 480,00</span></p>
+          <p class="parcelas">Parcele no cartão sem juros em 3x de R$ 160,00</p>
+          <a href="https://mpago.la/1DMNJq4" target="_blank" class="btn-compre">
+            Compre agora
+            <picture>
+              <source
+                srcset="~/assets/img/video/webp/img-mercado-pago.webp" type="image/webp" />
+              <source srcset="~/assets/img/video/img-mercado-pago.png" type="image/png" />
+              <img loading="lazy" src="~/assets/img/video/img-mercado-pago.png" alt />
+            </picture>
+          </a>
+        </div>
+        <div class="cell footer">
+          <a href="/">
+            <picture>
+              <source
+                srcset="~/assets/img/video/webp/marca-proposta.webp" type="image/webp" />
+              <source srcset="~/assets/img/video/marca-proposta.png" type="image/png" />
+              <img loading="lazy" src="~/assets/img/video/marca-proposta.png" alt />
+            </picture>
+          </a>
+          <p>
+            Av. Presidente Kennedy, 3425 | Água Verde <br/>
+            Curitiba-PR |  CEP: 80610-010 <br/>
+            CNPJ 20.012.672/0001-24
+          </p>
+          <p>
+            <a href="https://api.whatsapp.com/send?phone=5541988492754" target="_blank">
+              <span
+                class="iconify"
+                data-icon="mdi:whatsapp"
+                data-inline="false"
+              ></span>
+              41 98849.2754
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+
+.fundo-propostas { 
+  background-image: linear-gradient(to right, #422179 27% , #ffffff 0%);
+}
+.bg-propostas {
+  max-width: 1920px;
+  margin: 0 auto;
+  overflow: auto;
+  padding: 6%;
+  .video {
+
+  }
+  h1 {
+    margin-top: 70px;
+  }
+  .btn-compre {
+    background-color: #55ffc5;
+    border-radius: 5px;
+    border: 2px solid #55ffc5;
+    display: block;
+    font-size: 21px;
+    line-height: 54px;
+    max-width: 285px;
+    text-align: center;
+    &:hover {
+      background-color: transparent;
+      border: 2px solid #422179;
+      color: #422179;
+    }
+  }
+  .informacoes {
+    margin-top: 65px;
+    h3 {
+      color: #572b8c;
+      font-size: 18px;
+      margin-bottom: 20px;
+    }
+    p {
+      picture {
+        display: inline-block;
+        margin-right: 15px;
+        text-align: center;
+        width: 40px;
+      }
+      color: #572b8c;
+      font-size: 15px;
+      margin-bottom: 25px;
+    }
+    .formatos {
+      .mobile {
+        display: none;
+      }
+      p {
+        picture {
+          display: inline-block;
+          margin-right: 15px;
+          width: 47px;
+        }
+        color: #572b8c;
+        font-size: 15px;
+        margin-bottom: 5px;
+      }
+    }
+    .adquira {
+      p {
+        font-size: 15px;
+        margin: 0;
+        .traco {
+          text-decoration: line-through;
+        }
+        .por {
+          font-size: 20px;
+          font-weight: 700;
+          vertical-align: middle;
+        }
+      }
+      .parcelas {
+        font-size: 14px;
+        color: #484848;
+        margin-bottom: 30px;
+      }
+    }
+  }
+  .rodape {
+    margin-top: 100px;
+    picture {
+      display: block;
+      margin-bottom: 5px;
+    }
+    p {
+      font-size: 14px;
+      margin: 0;
+      a {
+        svg {
+          vertical-align: -2px;
+        }
+      }
+    }
+  }
+  .mobile-rodape {
+    display: none;
+  }
+}
+
+@media screen and (max-width: 1139px) {
+  .fundo-propostas { 
+    border-top: 30px solid #422179;
+    background-image: linear-gradient(to right, #ffffff 27% , #ffffff 0%);
+    .bg-propostas {
+      padding: 0;
+      .text-proposta {
+        display: none;
+      }
+    }
+    .proposta-criacao {
+      padding-left: 30px;
+      padding-right: 30px;
+      h1 {
+        font-size: 40px;
+        margin-top: 50px;
+      }
+      .informacoes {
+        margin-top: 35px;
+        .formatos {
+          font-size: 0;
+          margin-bottom: 40px;
+          p {
+            margin-bottom: 15px;
+          }
+          .desktop {
+            display: none;
+          }
+          .mobile {
+            display: block;
+          }
+          p {
+            display: inline-flex;
+            span {
+              display: none;
+            }
+          }
+          .tipos {
+            display: flex;
+            justify-content: space-between;
+            p {
+              &:last-child {
+                picture {
+                  margin: 0;
+                }
+              }
+            }
+          }
+        }
+      }
+      .adquira,
+      .rodape {
+        display: none;
+      }
+    }
+    .video {
+      background: #422179;
+      padding: 32px 30px 24px 30px;
+      order: 2;
+      iframe {
+        height: 700px;
+      }
+    }
+    .mobile-rodape {
+      display: block;
+      order: 3;      
+      text-align: center;
+      width: 100%;
+      .cell {
+        padding: 30px;
+      }
+      p {
+        color: #422179;
+        font-size: 21px;
+        margin: 0;
+        .traco {
+          text-decoration: line-through;
+        }
+        .por {
+          display: block;
+          font-size: 30px;
+          font-weight: 700;
+          vertical-align: middle;
+        }
+      }
+      .parcelas {
+        font-size: 16px;
+        color: #484848;
+        margin-bottom: 30px;
+      }
+      .btn-compre {
+        margin: 0 auto;
+      }
+      .footer {
+        background-color: #faf8f0;
+        text-align: left;
+        p {
+          color: #484848;
+          font-size: 16px;
+        }
+      }
+    }
+  }
+}
+
+@media screen and (max-width: 420px) {
+  .tipos {
+    display: block !important;
+    text-align: center;
+    p {
+      margin: 0 7px;
+      picture {
+        margin: 0 !important;
+      }
+    }
+  }
+}
+
+</style>
